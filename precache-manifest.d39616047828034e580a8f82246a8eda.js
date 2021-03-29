9cfd0ca815ea6cc4eb4112aa60596bab820e3282
Shubhamjain45 <60482023+Shubhamjain45@users.noreply.github.com>
2021-03-29 14:52:27 +0530

    Updates

---
+++ precache-manifest.d39616047828034e580a8f82246a8eda.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3975bfd5df5f3075c74711651fc9ddfc",
+    "url": "/Shubhamjain45/Netflix-Clone/index.html"
+  },
+  {
+    "revision": "0d41700ed8ab69bf57c8",
+    "url": "/Shubhamjain45/Netflix-Clone/static/css/1.c1c197bd.chunk.css"
+  },
+  {
+    "revision": "d0ffb927eba0c4d1178f",
+    "url": "/Shubhamjain45/Netflix-Clone/static/css/5.afea43fe.chunk.css"
+  },
+  {
+    "revision": "4375aee3ce79d625b35e",
+    "url": "/Shubhamjain45/Netflix-Clone/static/css/6.afea43fe.chunk.css"
+  },
+  {
+    "revision": "ba719252b92c4b4efecc",
+    "url": "/Shubhamjain45/Netflix-Clone/static/css/7.4ec44fee.chunk.css"
+  },
+  {
+    "revision": "793e4539d3a5e2d7b7b2",
+    "url": "/Shubhamjain45/Netflix-Clone/static/css/8.4ec44fee.chunk.css"
+  },
+  {
+    "revision": "5feec5b2f126c50d7ab7",
+    "url": "/Shubhamjain45/Netflix-Clone/static/css/main.50e19095.chunk.css"
+  },
+  {
+    "revision": "ce6dbf669ac772907c2f",
+    "url": "/Shubhamjain45/Netflix-Clone/static/js/0.47e39d0f.chunk.js"
+  },
+  {
+    "revision": "0d41700ed8ab69bf57c8",
+    "url": "/Shubhamjain45/Netflix-Clone/static/js/1.b1f451dd.chunk.js"
+  },
+  {
+    "revision": "c076a374b0dc032e2809",
+    "url": "/Shubhamjain45/Netflix-Clone/static/js/4.af00587b.chunk.js"
+  },
+  {
+    "revision": "d0ffb927eba0c4d1178f",
+    "url": "/Shubhamjain45/Netflix-Clone/static/js/5.be1fc8b5.chunk.js"
+  },
+  {
+    "revision": "4375aee3ce79d625b35e",
+    "url": "/Shubhamjain45/Netflix-Clone/static/js/6.5c07f01f.chunk.js"
+  },
+  {
+    "revision": "ba719252b92c4b4efecc",
+    "url": "/Shubhamjain45/Netflix-Clone/static/js/7.b567d9e4.chunk.js"
+  },
+  {
+    "revision": "793e4539d3a5e2d7b7b2",
+    "url": "/Shubhamjain45/Netflix-Clone/static/js/8.9d5d251e.chunk.js"
+  },
+  {
+    "revision": "5feec5b2f126c50d7ab7",
+    "url": "/Shubhamjain45/Netflix-Clone/static/js/main.1269d8ea.chunk.js"
+  },
+  {
+    "revision": "4af001940d7f12d078c3",
+    "url": "/Shubhamjain45/Netflix-Clone/static/js/runtime~main.072a956c.js"
+  },
+  {
+    "revision": "d6d290478902fbbe09c620b4aaba985f",
+    "url": "/Shubhamjain45/Netflix-Clone/static/media/AIMsqmnV.d6d29047.mp3"
+  },
+  {
+    "revision": "d2edb29f3f970c36aadecbb01ed0bb79",
+    "url": "/Shubhamjain45/Netflix-Clone/static/media/Background.d2edb29f.jpg"
+  },
+  {
+    "revision": "369787c5653cd811d0828096039cc933",
+    "url": "/Shubhamjain45/Netflix-Clone/static/media/cHBfBHfv.369787c5.mp3"
+  },
+  {
+    "revision": "3a36d79cdda4606c11565122b4edd691",
+    "url": "/Shubhamjain45/Netflix-Clone/static/media/cSWrcZcB.3a36d79c.png"
+  },
+  {
+    "revision": "f7f2904f1f3f39d9b1ff2400c30bcecc",
+    "url": "/Shubhamjain45/Netflix-Clone/static/media/imdb.f7f2904f.png"
+  },
+  {
+    "revision": "a1f673e7df715f16dae49f4874009082",
+    "url": "/Shubhamjain45/Netflix-Clone/static/media/logo.a1f673e7.png"
+  },
+  {
+    "revision": "d902c77253bec61f403a5978d2e7c858",
+    "url": "/Shubhamjain45/Netflix-Clone/static/media/netflix.d902c772.jpg"
+  },
+  {
+    "revision": "fd3ce8aa9f2030a21f7ba0724f58ce8c",
+    "url": "/Shubhamjain45/Netflix-Clone/static/media/rsRlAjMp.fd3ce8aa.png"
+  },
+  {
+    "revision": "9179862c82e20d655c43c9b468b96aa4",
+    "url": "/Shubhamjain45/Netflix-Clone/static/media/star.9179862c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e32b60b37c579e2a0c545a478105a67.js"
+  "/Shubhamjain45/Netflix-Clone/precache-manifest.d39616047828034e580a8f82246a8eda.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Shubhamjain45/Netflix-Clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
